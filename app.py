2788ddb94b5fa724d14d176a582e6884e22d6f3e
DAedemV <vidjrakouedem@gmail.com>
2021-12-08 01:58:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 st.set_page_config(layout='wide')
 
-marketing_campaign = pd.read_csv('C:/Users/53734R/OneDrive - MAIF/Bureau/Webdev/marketing_campaign.csv')
+marketing_campaign = pd.read_csv('marketing_campaign.csv')
 
 left_block, center_block, right_block = st.columns([2, 2, 2])
 
