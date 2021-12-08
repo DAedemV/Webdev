5a074ea68f7c9c59cda2a0bb37d1f940c83e463b
DAedemV <vidjrakouedem@gmail.com>
2021-12-08 07:42:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,17 +13,14 @@ markt_campaign.info()
 
 st.title('Dashboard campagne marketing')
 
-left_block, center_block, right_block = st.columns([2, 2, 2])
+left_block, right_block = st.columns([1,1])
 
 with left_block:
+    with st.expander('Cliquer pour ouvrir les paramètres de recherche')
     st.write('# LEFT BLOCK')
     first_container = st. container()
     with first_container:
         st.header('Mon premier container')
-
-with center_block:
-    st.write('# CENTER BLOCK')
-    st.header('Ton second container')
 
 with right_block:
     st.write('# RIGHT BLOCK')
