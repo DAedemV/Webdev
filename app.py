cd68e48f8ed7eb7009db878976a90f5440a0ffdc
DAedemV <vidjrakouedem@gmail.com>
2021-12-12 18:58:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,15 @@ st.set_page_config(layout='wide')
 
 ## Titre et organistaion du dashboard
 
-st.title('Dashboard campaing marketing')
+st.title('Dashboard marketing')
 
 left_block, right_block = st.columns([1, 1])
 
 ## Import des données
 
 df = pd.read_csv('marketing_campaign.csv', sheet_name='marketing_campaign.csv')
+
+
 
 
 
