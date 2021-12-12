d60320cf77335a9473bb1f2a17ea8c7a8b5f8ee1
DAedemV <vidjrakouedem@gmail.com>
2021-12-12 16:48:33 +0100

    edem_essaies

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ left_block, right_block = st.columns([1, 1])
 
 ## Import des données
 
-df = pd.read_excel('marketing_campaign.xls', sheet_name='marketing_campaign.xls')
+df = pd.read_csv('marketing_campaign.csv', sheet_name='marketing_campaign.csv')
 
 
 
