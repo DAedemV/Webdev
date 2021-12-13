747293a9205cea0d2178be517ad1415784b7076f
DAedemV <vidjrakouedem@gmail.com>
2021-12-13 13:42:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,21 +22,6 @@ left_block, right_block = st.columns([1, 1])
 # Chargement des données
 df = pd.read_excel('marketing_campaign.xls', sheet_name='marketing_campaign')
 
-#Transformer les données
-del df['Complain']
-#st.dataframe(df)
-
-# Création d'un nouveau JDD
-df2 = df.copy() 
-df2.reset_index(inplace=True)
-#st.dataframe(df2)
-
-#Calculer l'âge à partir de l'année de naissance
-age = []
-for i in df2['Year_Birth']:
-    age.append(2021 - i)
-
-df2['Age'] = age
 
 
 
